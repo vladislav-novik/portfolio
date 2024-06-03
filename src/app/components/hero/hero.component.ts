bc8db5e30fe42fa34a060c67147f6d633a837751
Novik_Vladislav <vladislavnovik95@gmail.com>
2024-06-03 22:37:26 +0300

    feat: Add hero component and styles

---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,29 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations'
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core'
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss',
+  animations: [
+    trigger('bannerTrigger', [
+      transition(":enter", [
+        query("*", [
+          style({ opacity: 0, transform: "translateX(-50px)" }),
+          stagger(50, [
+            animate(
+              "250ms cubic-bezier(0.35, 0, 0.25, 1)",
+              style({ opacity: 1, transform: "none" })
+            )
+          ])
+        ])
+      ])
+    ])
+  ]
+})
+export class HeroComponent {
+
+}
