85d2b1b108b6b5316de5c0a08826d6c80963e640
Novik_Vladislav <vladislavnovik95@gmail.com>
2024-06-05 00:18:57 +0300

    feat: add about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core'
+import { SectionTitleComponent } from '../section-title/section-title.component'
+import { SkillListComponent } from '../skill-list/skill-list.component'
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, SectionTitleComponent, SkillListComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
